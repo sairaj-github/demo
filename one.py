dc904fb6fc66a4f063c7a8731c7a445d304694c7
sairaj-github <sairajbhandari05@gmail.com>
2025-04-23 16:29:13 +0530

    initial commit

---
+++ one.py
@@ -0,0 +1,22 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Input must be a positive integer."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Print Fibonacci series up to n terms
+def print_fibonacci_series(terms):
+    if terms <= 0:
+        print("Please enter a positive integer.")
+    else:
+        print("Fibonacci series:")
+        for i in range(1, terms + 1):
+            print(fibonacci(i), end=" ")
+
+# Example usage
+n_terms = int(input("Enter the number of terms: "))
+print_fibonacci_series(n_terms)
